484a44d904a4eec7048fbd1c42ace206044f0260
Emil Gräs <emgr02002@utb.vaxjo.se>
2019-10-23 14:54:02 +0200

    Uppgift metoder 2 Bytplats på metoder

---
+++ uppgift 2 metoder/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace uppgift_2_metoder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 20;
+            int b = 10;
+            BytPlats(ref a, ref b);
+            Console.WriteLine(a + " " + b);
+        }
+        public static void BytPlats(ref int a, ref int b)
+        {
+            int byt = a;
+            a = b;
+            b = byt;
+
+        }
+    }
+}
